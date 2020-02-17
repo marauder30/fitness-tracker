9d4dffec1495918b06f5d8ce69817a420442a78e
Nick Koch <marauder30@gmail.com>
2020-02-17 14:32:55 -0700

    prepping heroku deploy

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const mysql = require("mysql");
+let connection;
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,7 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true});
+}
 
 app.use(require("./routes/apiRoutes.js"));
 app.use(require("./routes/htmlRoutes.js"));
