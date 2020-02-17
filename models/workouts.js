a2947e196f36079e715907acbbdbb8af60c528b0
Nick Koch <marauder30@gmail.com>
2020-02-17 13:36:05 -0700

    wrote html routes

--- models/workouts.js
+++ models/workouts.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
+const WorkoutSchema = new Schema({
     type: {
         type: String,
         trim: true,
@@ -30,6 +30,6 @@ const workoutSchema = new Schema({
     }
 });
 
-const Workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
